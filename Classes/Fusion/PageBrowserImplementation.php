1103e4fc0b85b497fd9fc1fda5d1bebb7aa21357
Patric Eckhart <mail@patriceckhart.com>
2020-04-09 17:30:22 +0200

    New templates and only fusion

--- Classes/Fusion/PageBrowserImplementation.php
+++ Classes/Fusion/PageBrowserImplementation.php
@@ -7,11 +7,12 @@ use Neos\Fusion\FusionObjects\AbstractFusionObject;
 class PageBrowserImplementation extends AbstractFusionObject {
 
     /**
-     * @return string
+     * @return array
      */
     public function evaluate() {
         $posts = $this->fusionValue('posts');
         $countposts = $this->fusionValue('countposts');
+        $page = $this->fusionValue('page');
 
         if (empty($posts)) {
             $itemsPerPage = 6;
@@ -20,55 +21,48 @@ class PageBrowserImplementation extends AbstractFusionObject {
         }
 
         $resultsCount = $countposts;
-        if ($resultsCount <= $itemsPerPage) {
-            $items = $articles;
-        } else {
-            $queryOffset = $itemsPerPage * ($page - 1);
-            $queryItems = $itemsPerPage * $page;
-            // $this->view->assign('queryOffset', $queryOffset);
-            // $filterString = '[blogcategories *= "';
-            // foreach ($category as $cat) {
-                // $filterString .= $cat->getIdentifier().',';
-            // }
-            // $filterString = trim($filterString, ',');
-            // $filterString .= '"]';
-            // $items = (new FlowQuery(array($node)))->children('[instanceof NeosRulez.Blog:BlogPost]')->context(array('workspaceName' => 'live'))->sort('_index', $sorting)->slice($queryOffset, $queryItems)->filter($filterString)->get();
-            $pages = ceil($resultsCount / $itemsPerPage);
-            if ($pages <= 10) {
-                $minPagination = 1;
+
+        if (empty($page)) {
+            $page = 1;
+        }
+
+        $pages = ceil($resultsCount / $itemsPerPage);
+        if ($pages <= 10) {
+            $minPagination = 1;
+            $maxPagination = $pages;
+        }
+        if ($pages > 10) {
+            $maxPagination = $page + 5;
+            if ($maxPagination > $pages) {
                 $maxPagination = $pages;
             }
-            if ($pages > 10) {
-                $maxPagination = $page + 5;
-                if ($maxPagination > $pages) {
-                    $maxPagination = $pages;
-                }
-                $minPagination = $maxPagination - 11;
-                if ($minPagination < 1) {
-                    $minPagination = 1;
-                    $maxPagination = 11;
-                }
-            }
-            for ($i = $minPagination; $i <= $maxPagination; $i++) {
-                $pagination['pages'][$i]['text'] = $i;
-            }
-            if ($page > 1) {
-                $pagination['prev'] = $page - 1;
-            }
-            if ($page < $pages) {
-                $pagination['next'] = $page + 1;
-            }
-            if ($minPagination > 1) {
-                $pagination['first'] = 1;
-            }
-            if ($maxPagination < $pages) {
-                $pagination['last'] = $pages;
+            $minPagination = $maxPagination - 11;
+            if ($minPagination < 1) {
+                $minPagination = 1;
+                $maxPagination = 11;
             }
         }
-        $pagination['current'] = $page;
-        // $this->view->assign('pagination', $pagination);
+        for ($i = $minPagination; $i <= $maxPagination; $i++) {
+            $pagination['pages'][$i]['text'] = $i;
+        }
+        if ($page > 1) {
+            $pagination['prev'] = $page - 1;
+        }
+        if ($page < $pages) {
+            $pagination['next'] = $page + 1;
+        }
+        if ($minPagination > 1) {
+            $pagination['first'] = 1;
+        }
+        if ($maxPagination < $pages) {
+            $pagination['last'] = $pages;
+        }
 
-        $result[] = ['pagination' => $pagination, 'queryOffset' => $queryOffset];
+        $queryOffset = $itemsPerPage * ($page - 1);
+        $queryItems = $itemsPerPage * $page;
+
+        $pagination['current'] = $page;
+        $result = ['pagination' => $pagination, 'queryOffset' => $queryOffset, 'queryItems'=> $queryItems, 'page' => $page];
 
         return $result;
     }
