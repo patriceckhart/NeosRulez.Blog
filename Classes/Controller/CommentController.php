c1de01a1a8c0368d3e98263a95f737d7ee3ec41c
patriceckhart <mail@patriceckhart.com>
2024-09-25 14:50:03 +0200

    fix: parentnode condition

--- Classes/Controller/CommentController.php
+++ Classes/Controller/CommentController.php
@@ -6,22 +6,24 @@ namespace NeosRulez\Blog\Controller;
  */
 
 use Neos\Flow\Annotations as Flow;
+use Neos\ContentRepository\Domain\Model\NodeTemplate;
+use Neos\ContentRepository\Domain\Service\ContextFactoryInterface;
+use Neos\ContentRepository\Domain\Service\NodeTypeManager;
 use Neos\Flow\Mvc\Controller\ActionController;
-use Neos\Eel\FlowQuery\FlowQuery;
-use Neos\Eel\FlowQuery\Operations;
+use Neos\SwiftMailer\Message;
 
 class CommentController extends ActionController
 {
 
     /**
      * @Flow\Inject
-     * @var \Neos\ContentRepository\Domain\Service\ContextFactoryInterface
+     * @var ContextFactoryInterface
      */
     protected $contextFactory;
 
     /**
      * @Flow\Inject
-     * @var \Neos\ContentRepository\Domain\Service\NodeTypeManager
+     * @var NodeTypeManager
      */
     protected $nodeTypeManager;
 
@@ -34,52 +36,54 @@ class CommentController extends ActionController
      * @param array $settings
      * @return void
      */
-    public function injectSettings(array $settings) {
+    public function injectSettings(array $settings): void
+    {
         $this->settings = $settings;
     }
 
-
-    public function createAction() {
-
-        $args = $this->request->getArguments()['args'];
-
+    /**
+     * @param array $args
+     * @return void
+     */
+    public function createAction(array $args): void
+    {
         $context = $this->contextFactory->create();
-        $newNode = new \Neos\ContentRepository\Domain\Model\NodeTemplate();
+        $newNode = new NodeTemplate();
         $newNode->setNodeType($this->nodeTypeManager->getNodeType('NeosRulez.Blog:Content.BlogPostComment'));
         $parentNode = $context->getNodeByIdentifier($args['parentnode']);
-        $childNodes = $parentNode->getChildNodes();
-        $commentNode = end($childNodes);
+        if($parentNode !== null) {
+            $childNodes = $parentNode->getChildNodes();
+            $commentNode = end($childNodes);
 
-        $now = new \DateTime();
-        $newNode->setProperty('blogPostCommentdate', $now);
-        $newNode->setProperty('blogPostCommentAuthorName',$args['name']);
-        $newNode->setProperty('blogPostCommentAuthorEmail',$args['email']);
-        $newNode->setProperty('blogPostCommentMessage',$args['message']);
-        $newNode->setProperty('title',$args['name']);
+            $now = new \DateTime();
+            $newNode->setProperty('blogPostCommentdate', $now);
+            $newNode->setProperty('blogPostCommentAuthorName', $args['name']);
+            $newNode->setProperty('blogPostCommentAuthorEmail', $args['email']);
+            $newNode->setProperty('blogPostCommentMessage', $args['message']);
+            $newNode->setProperty('title', $args['name']);
 
-        $commentNode->createNodeFromTemplate($newNode);
+            $commentNode->createNodeFromTemplate($newNode);
 
-        if(!empty($this->settings['notificationMail'])) {
-            $this->sendNotificationAction($_SERVER['HTTP_REFERER']);
+            if(!empty($this->settings['notificationMail'])) {
+                $this->sendNotificationAction($_SERVER['HTTP_REFERER']);
+            }
+
+            $this->redirectToUri($_SERVER['HTTP_REFERER'] . '#comments');
         }
-
-        $this->redirectToUri($_SERVER['HTTP_REFERER'].'#comments');
-
     }
 
     /**
      * @param string $url
      */
-    public function sendNotificationAction($url) {
-
-        $mail = new \Neos\SwiftMailer\Message();
+    public function sendNotificationAction(string $url): void
+    {
+        $mail = new Message();
         $mail
             ->setFrom(array($this->settings['notificationMail'] => $this->settings['notificationMail']))
             ->setTo(array($this->settings['notificationMail'] => $this->settings['notificationMail']))
             ->setSubject('Blog Notification');
         $mail->setBody('New comment on: '.$url, 'text/html');
         $mail->send();
-
     }
 
 }
